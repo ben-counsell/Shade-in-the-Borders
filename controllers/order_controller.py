7762ec61c12e3978bf0f7415ad610d947facdb3b
Ben <bencounsell@hotmail.co.uk>
2023-09-18 15:46:07 +0100

    improved /make_new_order, still working on it

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -20,16 +20,24 @@ def list_my_orders():
 def go_to_new_order_page():
     frames = Frame.query.all()
     fabrics = Fabric.query.all()
-    return render_template('make_new_order.jinja', frames=frames, fabrics=fabrics)
+    frame_styles = []
+    fabric_patterns = []
+    return render_template('make_new_order.jinja', frames=frames, fabrics=fabrics, frame_styles=frame_styles, fabric_patterns= fabric_patterns)
 
 @order_blueprint.route('/make_new_order', methods=['POST'])
-def place_new_order():
-    frame = request.form['frame_style']
-    fabric = request.form['fabric_pattern']
-    new_order = Order(frame_id=frame, fabric_id=fabric)
-    db.session.add(new_order)
-    db.session.commit()
-    return redirect('/my_orders')
+def make_new_order():
+    frame_style = request.form['frame_style']
+    frame_size = request.form['frame_size']
+    frames = Frame.query.all()
+    for frame in frames:
+        if frame.style == frame_style and frame.size == frame_size:
+            frame = Frame.query.filter(Frame.size == frame_size, Frame.style == frame_style).one()
+            fabric = request.form['fabric_pattern']
+            new_order = Order(frame_id=frame.id, fabric_id=fabric)
+            db.session.add(new_order)
+            db.session.commit()
+            return redirect('/my_orders')
+    return redirect('make_new_order.jinja')
 
 @order_blueprint.route('/admin')
 def log_in_as_admin():
